cb82a8cd1419baaf34d1be19e2af0e48ba79e63d
Mizpuno <nititorn.certihunt@gmail.com>
2023-08-01 15:39:18 +0700

    commit shit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LearnComponent } from './learn/learn.component';
 import { QuizComponent  } from './quiz/quiz.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: LearnComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'quiz', component: QuizComponent }
 ];
 
